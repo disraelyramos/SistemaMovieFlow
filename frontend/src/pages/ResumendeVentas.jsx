55c40e5c8ef74dd2ca26784d15bcb359ab68f897
christiandisra <109445267+christiandisraely@users.noreply.github.com>
2025-10-22 11:57:01 -0600

    actualizacion de resumenvista
    6

--- frontend/src/pages/ResumendeVentas.jsx
+++ frontend/src/pages/ResumendeVentas.jsx
@@ -1,7 +1,10 @@
 // src/views/CortedeCaja.jsx
 import React, { useMemo, useState, useEffect, useRef } from "react";
 import axios from "axios";
+// en src/pages/ResumendeVentas.jsx
 import "./styles/CortedeCaja.css";
+
+
 
 
 const API_BASE = import.meta.env.VITE_API_BASE_URL || "http://localhost:3001";
