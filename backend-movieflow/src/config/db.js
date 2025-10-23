f335f706961784e458a9b4f3bdf43376400aed64
christiandisra <109445267+christiandisraely@users.noreply.github.com>
2025-10-22 22:14:19 -0600

    actualizacion
    hoy

--- backend-movieflow/src/config/db.js
+++ backend-movieflow/src/config/db.js
@@ -1,16 +1,31 @@
-const oracledb = require('oracledb');
+const oracledb = require("oracledb");
+const path = require("path");
+const fs = require("fs");
 
 async function getConnection() {
   try {
+    // Carpeta del wallet (donde están tnsnames.ora y sqlnet.ora)
+    const walletPath = path.resolve(__dirname, "wallet");
+
+    if (!fs.existsSync(walletPath)) {
+      throw new Error(`⚠️ No se encontró el wallet en: ${walletPath}`);
+    }
+
+    // 👉 Clave: dile al driver dónde está el tnsnames.ora
+    process.env.TNS_ADMIN = walletPath;
+    console.log("DBG TNS_ADMIN =", process.env.TNS_ADMIN);
+
+    // Conecta usando el alias del tnsnames.ora
     const connection = await oracledb.getConnection({
-      user: process.env.ORACLE_USER,
-      password: process.env.ORACLE_PASSWORD,
-      connectString: process.env.ORACLE_CONNECT_STRING
+      user: "ADMIN",
+      password: "Movieflow202618Semitec",
+      connectString: "movieflownewpub_high",
     });
-    console.log('✅ Conectado exitosamente a Oracle');
+
+    console.log("✅ Conectado exitosamente a Oracle Cloud (Wallet)");
     return connection;
   } catch (err) {
-    console.error('❌ Error de conexión a Oracle:', err);
+    console.error("❌ Error de conexión a Oracle:", err);
     throw err;
   }
 }
