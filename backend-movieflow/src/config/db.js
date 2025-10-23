18521e16bd0ecc2cb4686fa54cc6222c54e4bad3
christiandisra <109445267+christiandisraely@users.noreply.github.com>
2025-10-23 14:08:06 -0600

    actualizaciones severas
    hoy

--- backend-movieflow/src/config/db.js
+++ backend-movieflow/src/config/db.js
@@ -1,10 +1,19 @@
 const oracledb = require('oracledb');
+require('dotenv').config();
 
 async function getConnection() {
-  return await oracledb.getConnection({
-    user: process.env.ORACLE_USER,
-    password: process.env.ORACLE_PASSWORD,
-    connectString: process.env.ORACLE_CONNECT_STRING, // movieflownewpub_high
-  });
+  try {
+    const connection = await oracledb.getConnection({
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      connectString: process.env.DB_CONNECT_STRING
+    });
+    console.log("✅ Conexión exitosa a Oracle");
+    return connection;
+  } catch (err) {
+    console.error("❌ Error de conexión a Oracle:", err);
+    throw err;
+  }
 }
+
 module.exports = { getConnection };
