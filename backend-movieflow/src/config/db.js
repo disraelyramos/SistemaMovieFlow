6d7505bf79e94696187fbb6ebe68d84292c2ac15
christiandisra <109445267+christiandisraely@users.noreply.github.com>
2025-10-22 21:09:49 -0600

    actualizaciones
    ahora

--- backend-movieflow/src/config/db.js
+++ backend-movieflow/src/config/db.js
@@ -2,14 +2,12 @@ const oracledb = require('oracledb');
 
 async function getConnection() {
   try {
-    const connectString = process.env.ORACLE_CONNECT_STRING;
-    const user = process.env.ORACLE_USER;
-    const password = process.env.ORACLE_PASSWORD;
+    const connectString = '(description=(retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1522)(host=adb.sa-bogota-1.oraclecloud.com))(connect_data=(service_name=g0fe86aee47d9c9_movieflownew_high.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))';
 
     const connection = await oracledb.getConnection({
-      user,
-      password,
-      connectString,
+      user: 'ADMIN',
+      password: 'Movieflow202618Semitec',
+      connectString
     });
 
     console.log('✅ Conectado exitosamente a Oracle Autonomous Database');
