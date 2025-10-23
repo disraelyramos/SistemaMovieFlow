6b8cd723e2c28d55faf29081d4ddf2c90a2383ca
christiandisra <109445267+christiandisraely@users.noreply.github.com>
2025-10-22 23:54:08 -0600

    actualizaciones de pruebas
    os

--- backend-movieflow/src/config/db.js
+++ backend-movieflow/src/config/db.js
@@ -1,9 +1,10 @@
 const oracledb = require('oracledb');
+
 async function getConnection() {
   return await oracledb.getConnection({
     user: process.env.ORACLE_USER,
     password: process.env.ORACLE_PASSWORD,
-    connectString: process.env.ORACLE_CONNECT_STRING, // <- alias
+    connectString: process.env.ORACLE_CONNECT_STRING, // movieflownewpub_high
   });
 }
 module.exports = { getConnection };
