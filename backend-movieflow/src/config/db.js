26f9763cb190525e21203f73c13c038c391fbf0d
christiandisra <109445267+christiandisraely@users.noreply.github.com>
2025-10-22 21:20:35 -0600

    actualizacion
    ahora

--- backend-movieflow/src/config/db.js
+++ backend-movieflow/src/config/db.js
@@ -2,7 +2,10 @@ const oracledb = require('oracledb');
 
 async function getConnection() {
   try {
-    const connectString = '(description=(retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1522)(host=adb.sa-bogota-1.oraclecloud.com))(connect_data=(service_name=g0fe86aee47d9c9_movieflownew_high.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))';
+    const connectString = `(description=(retry_count=20)(retry_delay=3)
+      (address=(protocol=tcps)(port=1522)(host=adb.sa-bogota-1.oraclecloud.com))
+      (connect_data=(service_name=g0fe86aee47d9c9_movieflownewpub_high.adb.oraclecloud.com))
+      (security=(ssl_server_dn_match=yes)))`;
 
     const connection = await oracledb.getConnection({
       user: 'ADMIN',
